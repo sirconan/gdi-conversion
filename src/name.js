b24bf6082a209b86697503eb6adea34dad454946
Damien Pichon <dp@deezer.com>
2021-07-12 22:59:32 +0200

    Fix name extraction: get first binary file name

--- src/name.js
+++ src/name.js
@@ -1,15 +1,23 @@
 #! /usr/bin/env node
 const fs = require('fs');
+const lineReader = require('line-reader');
 
-module.exports = function(workingDirectory) {
-	fs.open(`${workingDirectory}/track01.bin`, 'r+', function (inputFileError, inputFd) {
-		const buffer = Buffer.alloc(128);
-
-		fs.read(inputFd, buffer, 0, 128, 0x90, (error, bytesRead, bufferRead) => {
-			const outputNameFilePath = `${workingDirectory}/name.txt`;
-			fs.writeFile(outputNameFilePath, bufferRead.toString().trim(), () =>
-				console.log(`${outputNameFilePath} created`)
-			);
-		});
+module.exports = function(gdiPath, workingDirectory) {
+	lineReader.eachLine(gdiPath, (line) => {
+		if (line.charAt(0) === '1') {
+			const [,,,, binName] = line.split(' ');
+			
+			fs.open(`${workingDirectory}/${binName}`, 'r+', function (inputFileError, inputFd) {
+				const buffer = Buffer.alloc(128);
+		
+				fs.read(inputFd, buffer, 0, 128, 0x90, (error, bytesRead, bufferRead) => {
+					const outputNameFilePath = `${workingDirectory}/name.txt`;
+					fs.writeFile(outputNameFilePath, bufferRead.toString().trim(), () =>
+						console.log(`${outputNameFilePath} created`)
+					);
+				});
+			});
+		}
 	});
+	
 };
